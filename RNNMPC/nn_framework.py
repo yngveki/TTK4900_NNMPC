be6dd4bb668d8ed53a2a7d336384808155dbc1fb
Yngve Kippersund <ynge@kippersund.no>
2023-03-08 14:15:37 +0100

    Trained another model on similar dataset. Struggles badly with bias, but not much else

--- RNNMPC/nn_framework.py
+++ RNNMPC/nn_framework.py
@@ -50,12 +50,12 @@ class GroundTruth():
 # -- SETUP -- #
 TEST = True
 
-csv_path_train = Path(__file__).parent / 'generate_data/outputs/random_choke_ramp/csv/random_choke_ramp_1_globally_normalized_small_range.csv'
-csv_path_val = Path(__file__).parent / 'generate_data/outputs/random_choke_ramp/csv/random_choke_ramp_medium_1_globally_normalized_small_range.csv'
+csv_path_train = Path(__file__).parent / 'generate_data/outputs/random_choke_ramp/csv/random_choke_ramp_2_globally_normalized_small_range.csv'
+csv_path_val = Path(__file__).parent / 'generate_data/outputs/random_choke_ramp/csv/random_choke_ramp_medium_2_globally_normalized_small_range.csv'
 csv_path_test, test_save_name = Path(__file__).parent / 'generate_data/outputs/random_choke_ramp/csv/random_choke_ramp_short_1_globally_normalized_small_range.csv', 'random_choke_short'
 # csv_path_test, test_save_name = Path(__file__).parent / 'generate_data/outputs/random_choke/csv/random_choke_2_normalized_output_clipped.csv', 'random_choke_long'
 # csv_path_test, test_save_name = Path(__file__).parent / 'generate_data/outputs/steps_choke/csv/step_choke_50_52_output_clipped.csv', 'step_choke'
-model_nr = 2
+model_nr = 3
 model_name = "model_random_choke_ramp_" + str(model_nr)
 
 delta_t = 10
