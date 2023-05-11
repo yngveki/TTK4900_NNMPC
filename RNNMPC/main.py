13aa3bcbae7f177f182b3e1a4b3c17d4ed7e1160
Yngve Kippersund <ynge@kippersund.no>
2023-05-11 10:37:08 +0200

    Prepared for grid search light_weight_0_gridnr_2
    The reference values in ref_const_1 are chosen based off of the
    stationary values observed when choke is 100% and gas lift is 6000
    (l. 7000 in consistent_reachable_values_w_act.csv)

--- RNNMPC/main.py
+++ RNNMPC/main.py
@@ -80,12 +80,14 @@ for i, params in enumerate(sets):
     # if __name__ == '__main__':
         # BEST MODEL
         # model_name = 'model_grid_second_run_0'
+        if i <= -1:
+            continue
 
         # FASTER MODEL - still very decent (little to no offset?)
         model_name = 'light_weight_0'
         model_path = Path(__file__).parent / ('models/' + model_name + '/' + model_name + '.pt')
         fmu_path = Path(__file__).parent / 'fmu/fmu_endret_deadband.fmu'
-        ref_path = Path(__file__).parent / 'config/refs/refs2.csv'
+        ref_path = Path(__file__).parent / 'config/refs/ref_const_1.csv'
         nn_config_path = Path(__file__).parent / ('models/' + model_name + '/' + model_name + '.yaml')
         # fig_save_path = Path(__file__).parent / 'figs/mpc/test_fig.png'
         parent_dir = Path(__file__).parent / 'mpc_tunings'
@@ -162,10 +164,10 @@ for i, params in enumerate(sets):
         # -- SAVING FIGS -- #
         fig_dir = save_dir / ('figs/' + str(params['final_t'] // params['delta_t']) + '_steps')
         safe_save(fig_dir,
-                  fig,
-                  'fig',
-                  create_parent=True,
-                  errmsgstr=(str(params['final_t'] // params['delta_t']) + '_steps'))
+                    fig,
+                    'fig',
+                    create_parent=True,
+                    errmsgstr=(str(params['final_t'] // params['delta_t']) + '_steps'))
 
     except: # To safeguard against exiting on solution failure
         parent_dir = Path(__file__).parent / 'mpc_tunings'
